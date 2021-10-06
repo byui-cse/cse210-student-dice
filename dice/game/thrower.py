7385e43b2405eb2d32379c93a2c304d8c82d24de
ianmease8 <imease8@gmail.com>
2021-10-06 13:02:53 -0600

    yes

--- dice/game/thrower.py
+++ dice/game/thrower.py
@@ -1,3 +1,23 @@
 import random
 
-# TODO: Define the Thrower class here.
+class thrower:
+    def __init__(self):
+        dice = 5
+        num_throws = 0
+    
+    def can_throw(self):
+        if dice == 5 or dice == 1 or num_throws == 0:
+            play = True
+            return play
+        else: 
+            play = False
+            return play
+
+    def get_points(self):
+        if dice == 1:
+            points = points + 100
+        elif dice == 5:
+            points = points + 50
+
+    def throw_dice(self):
+        dice = []
